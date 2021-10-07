43af2057ca8a523fd544f393bfe290db907f0ce2
dimshadow101 <dimshadow101@penguin.lxd>
2021-10-07 14:30:27 -0700

    worker can be assigned cases now.

--- backend/worker/worker.go
+++ backend/worker/worker.go
@@ -3,7 +3,14 @@ package main
 import (
     "fmt"
     "net/http"
+	"log"
+	"github.com/gorilla/mux"
 )
+
+
+//Global Variable!!!!
+var assignedCases []supportCase 
+
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
@@ -21,31 +28,45 @@ func headers(w http.ResponseWriter, req *http.Request) {
 
 type supportCase struct {
 	CaseID string
-	State string
+	State string //State is the current case state 
 }
 
-func caseAssign(w http.ResponseWriter, req *http.Request, c []) {
-
-
+func calcNextTick() {
+	//Take the first assigned case, and figure out what to do with it. 
+	currentCase := assignedCases[0]
+	log.Println("Current case: ", currentCase)
+	return 
 }
 
-func caseAssign(w http.ResponseWriter, req *http.Request, ) {
+func caseAssign(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	// var caseId, ok = req.URL.Query()["caseid"]
+	// if !ok || len(caseId[0]) < 1 {
+    //     log.Println("Url Param 'caseid' is missing")
+    //     return 
+    // }
+	caseid := vars["caseid"]
+	c := supportCase{CaseID: caseid, State: "Assigned"}
+	assignedCases = append(assignedCases, c)
+	log.Println("case assigned: ", caseid)
+	fmt.Fprintf(w, "case accepted %s, assigned cases: %d \n", caseid, len(assignedCases))
 
-
+	//Trigger next work item recalc 
+	return 
 }
 
+// work on the next assigned thing. 
+func tick(w http.ResponseWriter, req *http.Request) {
 
+}
 
 func main() {
+	r := mux.NewRouter()
+    r.HandleFunc("/hello", hello)
+    r.HandleFunc("/headers", headers)
+	r.HandleFunc("/assign/{caseid}", caseAssign)
+	r.HandleFunc("/tick", tick)
 
-	cases := make([]supportCase, 20)
-
-	
-    http.HandleFunc("/hello", hello)
-    http.HandleFunc("/headers", headers)
-
-	http.handleFunc("/assign", caseAssign, cases)
-	http.handleFunc("/caseList", caseList, cases)	
-
+	http.Handle("/", r)
     http.ListenAndServe(":8080", nil)
 }
