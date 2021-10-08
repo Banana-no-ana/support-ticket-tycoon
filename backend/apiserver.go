c0af19045af661b59f907b0ff81f23ba043c336c
Ban Piao <bravo.alpha.new@gmail.com>
2021-10-07 20:59:48 -0700

    add a bunch of stub services to start thinking about how the system comes together

---
+++ backend/apiserver.go
@@ -0,0 +1,4 @@
+///API server is the primary interaction with frontend interms of "doing something"
+//Frontend says: assign cases from A to B, that's done by the API server. 
+//API server keeps all the state that frontend may need 
+//API server also responsible for adjusting game speed (controlled by tick rate)
