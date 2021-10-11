0d713c5fca4ecd40c80b91e316b34ba6e1526060
Ban Piao <bravo.alpha.new@gmail.com>
2021-10-11 13:42:44 -0700

    moved generate function to api server, and created list function.

--- backend/api.go
+++ backend/api.go
@@ -3,4 +3,53 @@
 //API server keeps all the state that frontend may need
 //API server also responsible for adjusting game speed (controlled by tick rate)
 
-//Server listens on 8081
+//Manually generate a case. Expose both RPC and REST endpoint, but they end up calling the same function.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var cases []Case
+var nextCaseId int //TODO: Change to use closure instead
+
+type Case struct {
+	CaseID   int
+	State    string //State is the current case state
+	Assignee string //worker UID.
+}
+
+func generate(w http.ResponseWriter, req *http.Request) {
+	c := Case{CaseID: nextCaseId, State: "New", Assignee: "Unassigned"}
+	cases = append(cases, c)
+	log.Println("case created: ", nextCaseId)
+	b, err := json.Marshal(c)
+	if err != nil {
+		log.Fatal("Failed to marshal caseID: ", nextCaseId)
+	}
+	fmt.Fprintf(w, string(b))
+	nextCaseId++
+}
+
+func list(w http.ResponseWriter, req *http.Request) {
+	b, err := json.Marshal(cases)
+	if err != nil {
+		log.Fatal("Failed to marshal caseID: ", nextCaseId)
+	}
+	fmt.Fprintf(w, string(b))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/generate", generate) //generate new case and return a case ID.
+	r.HandleFunc("/list", list)         //List all cases and their statuses
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8001", nil)
+}
