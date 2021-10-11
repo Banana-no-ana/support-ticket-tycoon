64e1e8035b937cbf1083a023e9872eff29fd9efc
Ban Piao <bravo.alpha.new@gmail.com>
2021-10-11 11:29:11 -0700

    - remove garbage file - removed some boilerplate from worker. Worker should behave the same as before. - started writing cases

--- backend/cases.go
+++ backend/cases.go
@@ -1,2 +1,41 @@
-//Master list of cases and case status. 
-//Does not tick
+//Master list of cases and case status.
+
+//Manually generate a case. Expose both RPC and REST endpoint, but they end up calling the same function.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var cases []Case
+var nextCaseId int //TODO: Change to use closure instead
+
+type Case struct {
+	CaseID string
+	State  string //State is the current case state
+}
+
+func generate(w http.ResponseWriter, req *http.Request) {
+	c := Case{CaseID: caseid, State: "Assigned"}
+	assignedCases = append(assignedCases, c)
+	log.Println("case assigned: ", caseid)
+	fmt.Fprintf(w, "case accepted %s, assigned cases: %d \n", caseid, len(assignedCases))
+
+	//Trigger next work item recalc
+	return
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/generate", generate) //generate new case and return a case ID.
+
+	http.Handle("/", r)
+
+	registerwithClock()
+	http.ListenAndServe(":8082", nil)
+}
--- backend/worker.go
+++ backend/worker.go
@@ -1,87 +1,71 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
 	"log"
+	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
-
 //Global Variable!!!!
-var assignedCases []supportCase 
+var assignedCases []Case
 
-
-func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "hello\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-    for name, headers := range req.Header {
-        for _, h := range headers {
-            fmt.Fprintf(w, "%v: %v\n", name, h)
-        }
-    }
-}
-
-type supportCase struct {
-	CaseID string
-	State string //State is the current case state 	
+type Case struct {
+	CaseID int
+	State  string //State is the current case state
 }
 
 func calcNextTick() {
-	//Take the first assigned case, and figure out what to do with it. 
+	//Take the first assigned case, and figure out what to do with it.
 	if len(assignedCases) == 0 {
 		return
 	}
 	currentCase := assignedCases[0]
 
 	switch currentCase.State {
-	case "Assigned" : //Just assigned. need to triage. 
-		return 
+	case "Assigned": //Just assigned. need to triage.
+		return
 	}
-	return 
+	return
 }
 
 func caseAssign(w http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
-	caseid := vars["caseid"]
-	c := supportCase{CaseID: caseid, State: "Assigned"}
+	caseid, _ := strconv.Atoi(vars["caseid"])
+	c := Case{CaseID: caseid, State: "Assigned"}
 	assignedCases = append(assignedCases, c)
 	log.Println("case assigned: ", caseid)
-	fmt.Fprintf(w, "case accepted %s, assigned cases: %d \n", caseid, len(assignedCases))
+	fmt.Fprintf(w, "case accepted %d, assigned cases: %d \n", caseid, len(assignedCases))
 
-	//Trigger next work item recalc 
-	return 
+	//Trigger next work item recalc
+	return
 }
 
-// work on the next assigned thing. 
+// work on the next assigned thing.
 func tick(w http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	tickNum := vars["ticknum"]
 	log.Println("We've been Ticked! Ticknum: ", tickNum)
 
-
-	//TODO: do the actual actions of the tick. 
+	//TODO: do the actual actions of the tick.
 	calcNextTick()
 	return
 }
 
-func registerwithClock() {	
-	//TODO: Figure out how to actually do this. 
+func registerwithClock() {
+	//TODO: Figure out how to actually do this.
 	return
 }
 
 func main() {
 	r := mux.NewRouter()
-    r.HandleFunc("/hello", hello)
-    r.HandleFunc("/headers", headers)
 	r.HandleFunc("/assign/{caseid}", caseAssign)
 	r.HandleFunc("/tick/{ticknum}", tick)
 
 	http.Handle("/", r)
 
 	registerwithClock()
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
